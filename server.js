4b7af74108b895e80d82be83d598d9bbc4fdd6a0
최은철 <wndqhrgkdlel@gmail.com>
2023-10-31 12:25:42 +0900

    소캣 연결을 위한 코드 수정

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const WebSocket = require('ws');
 const path = require('path');
 
-// 포트 번호 설정
-const port = 8000;
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// 정적 파일 제공 (index.html 파일을 포함)
-app.use(express.static(path.join(__dirname, 'public')));
+// 정적 파일 제공 (public 폴더 내의 파일)
+app.use(express.static(path.join(__dirname, 'public'));
 
-// 서버 시작
-app.listen(port, () => {
-  console.log(`localhost:${port}`);
+// WebSocket 연결을 설정합니다.
+wss.on('connection', (ws) => {
+  console.log('웹 소켓 연결이 열렸습니다.');
+
+  // 웹 소켓으로부터 메시지를 수신하고 클라이언트로 다시 전송
+  ws.on('message', (message) => {
+    console.log(`받은 메시지: ${message}`);
+    ws.send(`서버에서 다시 보낸 메시지: ${message}`);
+  });
+
+  // 연결이 닫힌 경우
+  ws.on('close', () => {
+    console.log('웹 소켓 연결이 닫혔습니다.');
+  });
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
 });
