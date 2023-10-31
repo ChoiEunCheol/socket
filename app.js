0ef1f9a1bd81167e43cdb024050033858aad0d66
최은철 <wndqhrgkdlel@gmail.com>
2023-10-31 12:10:09 +0900

    오타수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const port = 8000;
 
 // 정적 파일 제공 (index.html 파일을 포함)
-app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 서버 시작
 app.listen(port, () => {
